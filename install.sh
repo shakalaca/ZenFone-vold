daccf4e1bdb3b82e008d812e8791328f96d23a3c
Shaka Huang <shakalaca@gmail.com>
2016-06-02 16:59:19 +0800

    Update new patches
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 adb push assets/vold /data/local/tmp/vold
+adb push assets/libext2_blkid.so /data/local/tmp/libext2_blkid.so
 adb push assets/installer /data/local/tmp/installer
 adb shell "su -c chmod 755 /data/local/tmp/installer"
 adb shell "su -c /data/local/tmp/installer"
