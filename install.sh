56e2a5010815f9b4bbe21fcde34a752f2bf89094
Shaka Huang <shakalaca@gmail.com>
2016-06-04 23:04:38 +0800

    For arm64 5.0 init commit
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-adb push assets/vold /data/local/tmp/vold
-adb push assets/libext2_blkid.so /data/local/tmp/libext2_blkid.so
+adb push assets/bin/vold /data/local/tmp/vold
+adb push assets/bin/supolicy /data/local/tmp/supolicy
+adb push assets/bin/mkntfs /data/local/tmp/mkntfs
+adb push assets/bin/ntfsfix /data/local/tmp/ntfsfix
+adb push assets/bin/ntfs-3g /data/local/tmp/ntfs-3g
+adb push assets/lib64/libext2_blkid.so /data/local/tmp/libext2_blkid.so
 adb push assets/installer /data/local/tmp/installer
 adb shell "su -c chmod 755 /data/local/tmp/installer"
 adb shell "su -c /data/local/tmp/installer"
